5c6f1e1597c2c82d325cba0c3c727ecf340a01ee
Eliot A Heinrich <eliot.heinrich@gmail.com>
2024-01-02 22:43:40 -0500

    montecarlo is now hpp

--- include/MonteCarlo/MonteCarlo.hpp
+++ include/MonteCarlo/MonteCarlo.hpp
@@ -57,12 +57,46 @@ class MonteCarloSimulator : public dataframe::Simulator {
   // Most basic Monte-Carlo model to be simulated must have some notion of energy
   // as well as a mutation data structure. Specifics must be supplied by child classes.
   public:
-    MonteCarloSimulator(dataframe::Params &params, uint32_t num_threads);
+    MonteCarloSimulator(dataframe::Params &params, uint32_t num_threads) : dataframe::Simulator(params), num_threads(num_threads) {
+      final_temperature = dataframe::utils::get<double>(params, "temperature");
+      temperature = final_temperature;
+      init_temperature = dataframe::utils::get<double>(params, "initial_temperature", final_temperature);
+      num_cooling_updates = dataframe::utils::get<int>(params, "num_cooling_updates", 100);
+      cooling_schedule = parse_cooling_schedule(dataframe::utils::get<std::string>(params, "cooling_schedule", "constant"));
+    }
     virtual ~MonteCarloSimulator()=default;
 
-    // Implement Simulator methods but introduce MCModel methods
-    virtual void timesteps(uint32_t num_steps);
-    virtual void equilibration_timesteps(uint32_t num_steps);
+    // Implement Simulator methods but introduce MonteCarlo methods
+    virtual void timesteps(uint32_t num_steps) {
+      uint64_t num_updates = system_size()*num_steps;
+      for (uint64_t i = 0; i < num_updates; i++) {
+        generate_mutation();
+        double dE = energy_change();
+
+        double rf = randf();
+        if (rf < std::exp(-dE/temperature)) {
+          accept_mutation();
+        } else {
+          reject_mutation();
+        }
+      }
+    }
+
+    virtual void equilibration_timesteps(uint32_t num_steps) {
+      uint32_t steps_per_update = num_steps / num_cooling_updates;
+      temperature = init_temperature;
+      for (uint64_t i = 0; i < num_cooling_updates; i++) {
+        timesteps(steps_per_update);
+        switch (cooling_schedule) {
+          case(CoolingSchedule::Constant) : temperature = const_T(i, num_cooling_updates, init_temperature, final_temperature);
+          case(CoolingSchedule::Linear) : temperature = linear_T(i, num_cooling_updates, init_temperature, final_temperature);
+          case(CoolingSchedule::Trig) : temperature = trig_T(i, num_cooling_updates, init_temperature, final_temperature);
+        }
+      }
+
+      temperature = final_temperature;
+      timesteps(num_steps % num_cooling_updates);
+    }
 
     // To be overridden by child classes
     virtual double energy() const = 0;
